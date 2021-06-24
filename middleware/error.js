ddec989883484d1524cc362c31c18c1cbb3da4b7
Ayush Ashish <ayushashish0911@gmail.com>
2021-06-24 22:48:10 +0530

    Deploying to Heroku

--- middleware/error.js
+++ middleware/error.js
@@ -4,7 +4,6 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(error.statusCode || 500).json({
     success: false,
-    check: error.message,
     errors: [{ message: "Something went wrong." }],
   });
 };
